e3a1fe8a8d81222a9921f1163d7553b18886cbe7
David Fales <davidfalesct@gmail.com>
2022-01-08 20:54:10 -0700

    changed

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
 
 const express = require('express')
 const mongoose = require('mongoose')
@@ -6,13 +9,15 @@ const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', {
-  useNewUrlParser: true, useUnifiedTopology: true,
-})
-
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true })
+  const db = mongoose.connection
+  db.on('error', error => console.error(error))
+  db.once('open', () => console.log('connected to mongoose'))
 
 app.get('/', async (req, res) => {
   const articles = await Article.find().sort({ createdAt: 'desc' })
